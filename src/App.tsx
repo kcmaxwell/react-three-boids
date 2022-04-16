23529339ce31bbb7f0863ebd1f11f4abb002e3f3
Kristopher Maxwell <zerotrig@gmail.com>
2022-04-16 12:30:16 -0600

    Add basic setup for @react-three/fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import "./styles.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Edges } from "@react-three/drei";
+import { Suspense, useRef } from "react";
+
+const Box = () => {
+  const mesh = useRef();
+
+  useFrame((state, delta) => {
+    //mesh.current.rotation.y += 1 * delta;
+  });
+
+  return (
+    <mesh ref={mesh} scale={[2, 2, 2]}>
+      <boxBufferGeometry args={[3, 1.5, 3]} />
+      <meshPhongMaterial visible={false} />
+      <Edges />
+    </mesh>
+  );
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Canvas>
+        <Suspense fallback={"Loading..."}>
+          <OrbitControls />
+          <Box />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
