1f319ba023c30856c0258b97cf0af6b16b237488
Kristopher Maxwell <zerotrig@gmail.com>
2022-04-21 15:25:18 -0600

    Create Boids and Boid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import "./styles.css";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Edges } from "@react-three/drei";
+import * as THREE from "three";
+import { Canvas, useFrame, MeshProps } from "@react-three/fiber";
+import { OrbitControls, Edges, Instances, Instance } from "@react-three/drei";
 import { Suspense, useRef } from "react";
+import { AmbientLight } from "three";
+
+const randomPosition = (r: number) =>
+  new THREE.Vector3(
+    r / 2 - Math.random() * r,
+    r / 2 - Math.random() * r,
+    r / 2 - Math.random() * r
+  );
+
+const initialPositions = Array.from({ length: 10 }, () => ({
+  position: randomPosition(0)
+}));
 
 const Box = () => {
-  const mesh = useRef();
+  const mesh = useRef<THREE.Mesh>();
 
   useFrame((state, delta) => {
     //mesh.current.rotation.y += 1 * delta;
@@ -19,12 +32,37 @@ const Box = () => {
   );
 };
 
+const Boid = (props: MeshProps) => {
+  const mesh = useRef();
+
+  return (
+    <group {...props}>
+      <Instance ref={mesh} />
+    </group>
+  );
+};
+
+const Boids = () => {
+  return (
+    <Instances limit={10}>
+      <coneBufferGeometry args={[0.1, 0.3, 24]} />
+      <meshPhongMaterial color="#099AD9" />
+      {initialPositions.map((props, i) => (
+        <Boid key={i} {...props} />
+      ))}
+    </Instances>
+  );
+};
+
 export default function App() {
   return (
     <div className="App">
       <Canvas>
         <Suspense fallback={"Loading..."}>
+          <pointLight position={[10, 10, 10]} />
+          <ambientLight />
           <OrbitControls />
+          <Boids />
           <Box />
         </Suspense>
       </Canvas>
